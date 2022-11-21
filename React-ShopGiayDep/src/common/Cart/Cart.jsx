dc4799188979414f8b7bb90be72fb600bd728b7d
LNA02 <89622960+LNA02@users.noreply.github.com>
2022-11-21 18:40:29 +0700

    update

--- React-ShopGiayDep/src/common/Cart/Cart.jsx
+++ React-ShopGiayDep/src/common/Cart/Cart.jsx
@@ -2,20 +2,20 @@ import React from "react"
 import "./style.css"
 
 const Cart = ({ CartItem, addToCart, decreaseQty }) => {
- 
+  // Stpe: 7   calucate total of items
   const totalPrice = CartItem.reduce((price, item) => price + item.qty * item.price, 0)
 
-
+  // prodcut qty total
   return (
     <>
       <section className='cart-items'>
         <div className='container d_flex'>
-   
+          {/* if hamro cart ma kunai pani item xaina bhane no diplay */}
 
           <div className='cart-details'>
-            {CartItem.length === 0 && <h1 className='no-items product'>No Items are add in Cart</h1>}
+            {CartItem.length === 0 && <h1 className='no-items product'>Không có sản phẩm nào trong giỏ hàng</h1>}
 
-       
+            {/* yasma hami le cart item lai display garaaxa */}
             {CartItem.map((item) => {
               const productQty = item.price * item.qty
 
@@ -37,7 +37,9 @@ const Cart = ({ CartItem, addToCart, decreaseQty }) => {
                         <i className='fa-solid fa-xmark'></i>
                       </button>
                     </div>
-               
+                    {/* stpe: 5 
+                    product ko qty lai inc ra des garne
+                    */}
                     <div className='cartControl d_flex'>
                       <button className='incCart' onClick={() => addToCart(item)}>
                         <i className='fa-solid fa-plus'></i>
@@ -55,9 +57,9 @@ const Cart = ({ CartItem, addToCart, decreaseQty }) => {
           </div>
 
           <div className='cart-total product'>
-            <h2>Cart Summary</h2>
+            <h2>Tổng:</h2>
             <div className=' d_flex'>
-              <h4>Total Price :</h4>
+              <h4>Tổng giá tiền :</h4>
               <h3>${totalPrice}.00</h3>
             </div>
           </div>
