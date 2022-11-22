c0207cc7b85f5695aeb74f60d12c2381a025db6f
PhamQuocHao53375 <89623220+PhamQuocHao53375@users.noreply.github.com>
2022-11-22 17:20:06 +0700

    Update Cart.jsx

--- React-ShopGiayDep/src/common/Cart/Cart.jsx
+++ React-ShopGiayDep/src/common/Cart/Cart.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import "./style.css"
 
 const Cart = ({ CartItem, addToCart, decreaseQty }) => {
-  // Stpe: 7   calucate total of items
   const totalPrice = CartItem.reduce((price, item) => price + item.qty * item.price, 0)
 
   // prodcut qty total
@@ -10,12 +9,9 @@ const Cart = ({ CartItem, addToCart, decreaseQty }) => {
     <>
       <section className='cart-items'>
         <div className='container d_flex'>
-          {/* if hamro cart ma kunai pani item xaina bhane no diplay */}
 
           <div className='cart-details'>
             {CartItem.length === 0 && <h1 className='no-items product'>Không có sản phẩm nào trong giỏ hàng</h1>}
-
-            {/* yasma hami le cart item lai display garaaxa */}
             {CartItem.map((item) => {
               const productQty = item.price * item.qty
 
@@ -37,9 +33,6 @@ const Cart = ({ CartItem, addToCart, decreaseQty }) => {
                         <i className='fa-solid fa-xmark'></i>
                       </button>
                     </div>
-                    {/* stpe: 5 
-                    product ko qty lai inc ra des garne
-                    */}
                     <div className='cartControl d_flex'>
                       <button className='incCart' onClick={() => addToCart(item)}>
                         <i className='fa-solid fa-plus'></i>
